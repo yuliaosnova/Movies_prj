9bf108868973b3b92454cca54cd405c0550bc2cf
Yulia Osnova <yulia.osnova@gmail.com>
2023-06-14 10:55:43 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -4,22 +4,15 @@ import "./index.css";
 
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistor, store } from "./redux/store";
-
+import { store } from "./redux/store";
+import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
+  <React.StrictMode>
     <BrowserRouter basename="/">
-      
-        <Provider store={store}>
-          {/* <PersistGate loading={null} persistor={persistor}> */}
-	
-            <App />
-			
-          {/* </PersistGate> */}
-        </Provider>
-      
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
-//   </React.StrictMode>
+  </React.StrictMode>
 );
