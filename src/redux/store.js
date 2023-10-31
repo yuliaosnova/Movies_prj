c6756fc1965bc54c844b46c27b74ea8d578bb0bc
Yulia Osnova <yulia.osnova@gmail.com>
2023-10-31 14:48:37 +0200

    FIXED: remove some comments

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,7 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-// import { collectionReducer } from "./collectedMovieSlice";
 
 import {
   persistStore,
-  persistReducer,
   FLUSH,
   REHYDRATE,
   PAUSE,
@@ -11,24 +9,15 @@ import {
   PURGE,
   REGISTER,
 } from "redux-persist";
-import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import { genresApi } from "./genresSlice";
 import { userSlice } from "./userSlice";
 
-// const persistConfig = {
-//   key: "root",
-//   version: 1,
-//   storage,
-//   whitelist: ['collectedMovies'], 
-// };
 
 const rootReducer = combineReducers({
-//   collectedMovies: collectionReducer,
   [genresApi.reducerPath]: genresApi.reducer,
   user: userSlice.reducer,
 });
 
-// const persistedReduser = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: rootReducer,
